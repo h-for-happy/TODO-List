e29cdc3290b418159a6a87aa4d7746b956194310
Hitesh Taneja <hiteshtaneja307@gmail.com>
2021-05-15 02:24:13 +0530

    add function done

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -2,12 +2,25 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  const [todos, setTodos]=useState(["Complete this course", "add it to git up"]);
+  const [todos, setTodos]=useState(["Complete this course", "add it to git up","test add"]);
+  const [input, setInput]=useState("");
+  console.log("--+",input);
+
+  const addTodo = (event) => {
+    //this will add the todo in out list or append that in our array
+    event.preventDefault(); // will stop the refresh
+    console.log("+-+" , "Added in the list");
+    setTodos([...todos, input]);
+    setInput("");//clear the input after we click submit
+  }
   return (
     <div className="App">
       <h1>Hello World i am happy</h1>
-      <input/>
-      <button>Add todo</button>
+      <form>
+        <input value={input} onChange={event => setInput(event.target.value)}/>
+        <button type="submit" onClick={addTodo}>Add todo</button>
+      </form>
+      
       {todos.map(todo => (
         <li>{todo}</li>
       ))}
