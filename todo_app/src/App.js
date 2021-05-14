bf7ba51e85881f1c2ae2c1bee0d81d94de2e50bd
Hitesh Taneja <hiteshtaneja307@gmail.com>
2021-05-15 01:56:21 +0530

    firebase setup ,starting with functionality

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos]=useState(["Complete this course", "add it to git up"]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World i am happy</h1>
+      <input/>
+      <button>Add todo</button>
+      {todos.map(todo => (
+        <li>{todo}</li>
+      ))}
     </div>
   );
 }
